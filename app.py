afbbf569a74c9151341d7b51345798a02c58d48c
Rafa Wierzba <rafaelwierzba@gmail.com>
2024-02-04 14:15:37 -0300

    Update app.py
    Juntando parte feita pela Ashley (botões).

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import tkinter 
+import tkinter
 from tkinter import *
 from tkinter import ttk
 
@@ -30,6 +30,23 @@ frame_cima.grid(row=0, column=0, sticky=NW, padx=10, pady=10)
 frame_baixo = Frame(janela, width=240, height=300, bg=fundo, relief="flat")
 frame_baixo.grid(row=1, column=0, sticky=NW)
 
+# Criando logica do app ------------------------------
+
+Jogador_1 = "X"
+Jogador_2 = "O"
+
+score_1 = "X"
+score_2 = "O"
+
+tabela = [["1", "2", "3"] ,["4" ,"5" ,"6"], ["7" ,"8" ,"9"]]
+
+def iniciar_jogo():
+    #para controlar o jogo
+    def controlar():
+        pass
+        #para decidir o vencedor
+        def vencedor():
+          
 # CONFIGURANDO O FRAME DE CIMA (CABEÇALHO)
 app_x = Label(frame_cima, text="X", height=1,
               relief='flat', anchor="center", 
@@ -80,20 +97,46 @@ app_.place(x=90, y=15)
 app_ = Label(frame_baixo, text="", height=23,
               relief='flat', pady="5", anchor="center", 
               font=("Ivy 5 bold"), bg=co0, fg=co7)
-app_.place(x=160, y=15)
+app_.place(x=156, y=15)
 
 # LINHAS HORIZONTAIS
-app_ = Label(frame_baixo, text="", width=48,
+app_ = Label(frame_baixo, text="", width=46,
               relief='flat', padx="2", pady="1", anchor="center", 
               font=("Ivy 5 bold"), bg=co0, fg=co7)
 app_.place(x=30, y=63)
 
-app_ = Label(frame_baixo, text="", width=48,
+app_ = Label(frame_baixo, text="", width=46,
               relief='flat', padx="2", pady="1", anchor="center", 
               font=("Ivy 5 bold"), bg=co0, fg=co7)
-app_.place(x=30, y=123)
+app_.place(x=30, y=127)
+
+#linha 0 de botões  
+b_0 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_0.place(x=30, y=15)
+b_1 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_1.place(x=96, y=15)
+b_2 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_2.place(x=162, y=15)
+
+#linha 1 de botões
+b_3 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_3.place(x=30, y=75)
+b_4 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_4.place(x=96, y=75)
+b_5 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_5.place(x=162, y=75)
 
 
+#linha 2 de botões
+b_6 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_6.place(x=30, y=135)
+b_7 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_7.place(x=96, y=135)
+b_8 = Button(frame_baixo, text=" ", width=3, height=1,  font=("Ivy 20 bold"), overrelief=RIDGE, relief="flat", bg=fundo, fg=co7)
+b_8.place(x=162, y=135)
 
+# botão jogar 
+b_jogar = Button(frame_baixo, text="Jogar ", width=10, height=1,  font=("Ivy 10 bold"), overrelief=RIDGE, relief="raised", bg=fundo, fg=co0)
+b_jogar.place(x=81, y=210)
 
 janela.mainloop()
